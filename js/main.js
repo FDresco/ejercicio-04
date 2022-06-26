9f77d905da2bf7c5242d3aec1aa221d62f4d0210
Florencia Dresco <florenciadresco@gmail.com>
2022-06-26 12:10:47 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* let nombre = prompt("Ingrese su nombre")
+
+let genero = prompt("Ingrese su genero: Mujer - Hombre - No binario")
+
+
+if (genero = "Mujer") {
+    console.log("Bienvenida " + nombre)   
+} else if (genero = "Hombre") {
+    console.log("Bienvenido " + nombre)
+} else {
+    console.log("Bienvenide " + nombre)
+}
+ */
